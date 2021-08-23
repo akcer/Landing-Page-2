0db6b62394c58b88d2801a90c3cd403017c77398
Jarek Hryniewicki <jarhryniewicki@gmail.com>
2021-08-23 22:42:04 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import 'bootstrap';
+import style from "./style.scss"; 
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  output: {
+    clean:true,
+  },
+  devServer: {
+    port: 9000,
+    static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    compress: true,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env']],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+};
